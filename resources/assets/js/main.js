6818f3ef26015ba793c6d5d9ef743b74ffbe6877
MehdiHenna <medh31@gmail.com>
2016-05-09 22:47:39 +0200

    tentative de faire fonctionné codemirror avec jquery ajout du fichier jquery dans le public/js

--- resources/assets/js/main.js
+++ resources/assets/js/main.js
@@ -1,15 +1,7 @@
+$(document.ready(function(){
 
-var React = require('react');
-var ReactDOM = require('react-dom');
-
-
-ReactDOM.render(
-  <h1>Hello</h1>,
-  document.getElementById('hello')
-);
-
-
-var myCodeMirror = CodeMirror(document.getElementById(input-user), {
-	value: "function myScript(){return 100;}\n",
-	mode: "markdown"
+var code = $(".container")[0];
+var editor = CodeMirror.fromTextArea(code, {
+	lineNumbers : true
+	});
 });
