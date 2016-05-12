5ed35c08ddc3dcb1a4c5aa64e3c37577bea3e893
MehdiHenna <mehdihennaoui@gmail.com>
2016-05-12 15:56:33 +0200

    Ajout des commentaires dans index.html et main.js

--- resources/assets/js/main.js
+++ resources/assets/js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 	var marked = require('marked'); 
 	var block_code = document.getElementById('block-code');
-	var translation_result = document.getElementById('translation-result');
+	// insert codeMirror dans le textArea block-code
 	var editor = CodeMirror.fromTextArea(block_code, {
 		mode: "markdown",
 		lineNumbers : true,
@@ -10,7 +10,7 @@ $(document).ready(function(){
 		extraKeys: {"Enter": "newlineAndIndentContinueMarkdownList"}
 
 	});
-
+	//  traduit le code de textArea et fait une previsualiton automatiquement lorsque l'utilisateur tape son code 
 	editor.on('change', function(){
 		$('#translation-result').html(marked(editor.getValue()));
 	});
