d01a2f75696403eeabe8de5aeea7a7b7443d6f88
MehdiHenna <mehdihennaoui@gmail.com>
2016-05-12 16:25:17 +0200

    enlever les espaces inutiles dans main.js

--- resources/assets/js/main.js
+++ resources/assets/js/main.js
@@ -8,12 +8,9 @@ $(document).ready(function(){
 		lineWrapping: true,
 		theme: "default",
 		extraKeys: {"Enter": "newlineAndIndentContinueMarkdownList"}
-
 	});
 	//  traduit le code de textArea et fait une previsualiton automatiquement lorsque l'utilisateur tape son code 
 	editor.on('change', function(){
 		$('#translation-result').html(marked(editor.getValue()));
 	});
-
-
 });
