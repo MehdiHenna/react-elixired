c27d650e0e3b609b00a6fa5ff49698c3004981dc
MehdiHenna <mehdihennaoui@gmail.com>
2016-06-03 14:33:04 +0200

    Mise en forme de la page

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -1293,16 +1293,15 @@ if (typeof module !== 'undefined' && typeof exports === 'object') {
 $(document).ready(function () {
 	var marked = require('marked');
 	var block_code = document.getElementById('block-code');
-	var translation_result = document.getElementById('translation-result');
+	// insert codeMirror dans le textArea block-code
 	var editor = CodeMirror.fromTextArea(block_code, {
 		mode: "markdown",
 		lineNumbers: true,
 		lineWrapping: true,
 		theme: "default",
 		extraKeys: { "Enter": "newlineAndIndentContinueMarkdownList" }
-
 	});
-
+	//  traduit le code de textArea et fait une previsualiton automatiquement lorsque l'utilisateur tape son code
 	editor.on('change', function () {
 		$('#translation-result').html(marked(editor.getValue()));
 	});
